ac9bb55e6e8213082e465487a36cde80a8608f5a
Yugen1121 <yugenlimbu777@gmail.com>
2026-02-01 20:03:23 +0000

    Added some theme and strings

--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
